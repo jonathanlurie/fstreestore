04fe2eb44ef71d6e4e075b77749e38099705680e
jonathanlurie <lurie.jo@gmail.com>
2020-05-11 18:13:12 +0200

    ADD remove key-value including deleting dead branch

--- example/01.js
+++ example/01.js
@@ -8,6 +8,16 @@ let s = new Store('myStore01')
 
 async function main() {
   await s.init()
+  
+  console.time('c')
+  // await s.set('some_key', 'all right')
+  // await s.set('some_other_key', 'all right0')
+  // await s.set('some', 'all right2')
+  // console.log(await s.get('some_key'))
+  await s.remove('some_other_key')
+  
+  // console.log('has some_other_', await s.has('some_other_'));
+  console.timeEnd('c')
 }
 
 main()
--- src/Store.js
+++ src/Store.js
@@ -1,11 +1,13 @@
 const path = require('path')
-// const fs = require('promise-fs')
+const rmfr = require('rmfr');
 const fs = require('fs').promises
+const FtsCodec = require('./FtsCodec')
 
 
-const VALID_KEY_CHAR = 'abcdefghijklmnopqstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_'
+const VALID_KEY_CHAR = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_'
 const MAX_KEY_LENGTH = 36
 const VALUE_FILE_NAME = 'value.fts'
+
 
 class Store {
 
@@ -15,6 +17,7 @@ class Store {
 
 
   async init() {
+    console.log('here')
     try {
       await fs.access(this._path)
     } catch(err) {
@@ -36,6 +39,8 @@ class Store {
 
     for (let i = 0; i < key.length; i += 1) {
       if (!VALID_KEY_CHAR.includes(key[i])) {
+        console.log(key[i])
+        
         return false
       }
     }
@@ -58,7 +63,7 @@ class Store {
    * @param {*} key 
    */
   async has (key) {
-    if(!isValidKey(key)) {
+    if(!this.isValidKey(key)) {
       throw new Error(`The key must contain only alphanumeric, dash and underscore characters. The length cannot be greater than ${MAX_KEY_LENGTH}. The key ${VALUE_FILE_NAME} is reserved.`)
     }
 
@@ -73,9 +78,9 @@ class Store {
     }
   }
 
-
+  
   async remove (key) {
-    if(!isValidKey(key)) {
+    if(!this.isValidKey(key)) {
       throw new Error(`The key must contain only alphanumeric, dash and underscore characters. The length cannot be greater than ${MAX_KEY_LENGTH}. The key ${VALUE_FILE_NAME} is reserved.`)
     }
 
@@ -83,38 +88,101 @@ class Store {
     let valuePath = path.join(keyPath, VALUE_FILE_NAME)
 
     try {
+
+      // removing the value file
       await fs.unlink(valuePath)
 
-      // then, we must check if there are other sub dirs in the key dir
-      // (keys that would start with the same string)
-      // if so, we don't do any additional task, if not, we have to delete this branch of the
-      // tree up to the place where there are other values.
-      const keyDirChild = await fs.readDir(keyPath, {withFileTypes: true})
-      let dirChildren = keyDirChild.filter(child => child.isDirectory())
-      if(dirChildren.length === 0) {
-        // TODO: delete the branch here
+      let pathsToRoot = []
+      for (let i = 1; i <= key.length; i += 1) {
+        pathsToRoot.push(
+          this._keyToPath(key.slice(0, i))
+        )
       }
-    } catch(err) {}
+      // paths from the closest to the removed value up to the root
+      pathsToRoot = pathsToRoot.reverse()
+
+      // we are now deleting the branch up until its no longer a dead branch
+
+      async function deleteBranch(paths, index) {
+        const keyDirChild = await fs.readdir(paths[index], {withFileTypes: true})
+        let valuableContent = keyDirChild.filter(child => (child.isDirectory() || child.name === VALUE_FILE_NAME))
+        
+        if (valuableContent.length === 0) {
+          await rmfr(paths[index])
+
+          if (index < paths.length - 1) {
+            await deleteBranch(paths, index += 1)
+          } else {
+            return
+          }
+          
+        } else {
+          return
+        }
+      }
+
+      await deleteBranch(pathsToRoot, 0)
+      
+    } catch(err) {
+      console.error(err)
+    }
   }
 
 
-
-
-  async put (key, value) {
-    if(!isValidKey(key)) {
+  async set (key, value) {
+    if(!this.isValidKey(key)) {
       throw new Error(`The key must contain only alphanumeric, dash and underscore characters. The length cannot be greater than ${MAX_KEY_LENGTH}. The key ${VALUE_FILE_NAME} is reserved.`)
     }
 
     let keyPath = this._keyToPath(key)
+    let valuePath = path.join(keyPath, VALUE_FILE_NAME)
+
+    // creating the key folder if it does not exist
+    try {
+      const stat = await fs.lstat(keyPath)
+    } catch(err) {
+      await fs.mkdir(keyPath, {recursive: true})
+    }
+
+    // create the value file (no matter if it exists)
+    let ftsBuff = FtsCodec.encode(value)
+    await fs.writeFile(valuePath, Buffer.from(ftsBuff), 'binary')
   }
 
 
-  async get (key) {
-    if(!isValidKey(key)) {
+  async get (key, options = {}) {
+    if(!this.isValidKey(key)) {
       throw new Error(`The key must contain only alphanumeric, dash and underscore characters. The length cannot be greater than ${MAX_KEY_LENGTH}. The key ${VALUE_FILE_NAME} is reserved.`)
     }
 
+    const mustThrow = 'throw' in options ? options.throw : true
+
+    let exists = false
+    let errorMessage = ''
+
     let keyPath = this._keyToPath(key)
+    let valuePath = path.join(keyPath, VALUE_FILE_NAME)
+
+    try {
+      const stat = await fs.lstat(valuePath)
+      if (stat.isFile()) {
+        exists = true
+      }
+    } catch(err) {
+      errorMessage = `No record with the key ${key}`
+    }
+
+
+    if (exists) {
+      let buf = new Uint8Array(await fs.readFile(valuePath)).buffer
+      return FtsCodec.decode(buf, {throw: mustThrow})
+    } else {
+      if (mustThrow) {
+        throw new Error(errorMessage)
+      } else {
+        return null
+      }
+    }
   }
 }
 
